c8ea6ee044d79bcffd65d566cd71ace0d00a40a5
Ruilin Xu <rx2132@columbia.edu>
2023-09-20 00:16:57 -0400

    add read badge

--- scripts/ui_bookshelf.js
+++ scripts/ui_bookshelf.js
@@ -211,10 +211,22 @@ var bookshelf = {
         let progress = getProgressText(fname, false);
         if (progress) {
             bookElm.addClass("read").css("--read-progress", progress);
-            bookElm.find(".progress").html(progress).attr("title", progress);
+            if (progress === "100%") {
+                bookElm.find(".progress").html(style.ui_bookFinished).attr("title", progress);
+
+                // add a badge to the book cover
+                let badge = `<div class="bookFinished_badge">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z" />
+                        </svg>
+                    </div>`;
+                bookElm.find(".coverContainer").append(badge);
+            } else {
+                bookElm.find(".progress").html(progress).attr("title", progress);
+            }
         } else {
             bookElm.removeClass("read").css("--read-progress", "");
-            bookElm.find(".progress").html("0%");
+            bookElm.find(".progress").html(style.ui_bookNotRead);
         }
     },
 
